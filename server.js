c0ff06c3da672a8c091a27bfa867bf6f2a890569
Jeff Grant <jgrant274@gmail.com>
2021-07-18 23:35:15 -0400

    setting up routes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//setting dependencies
 const express = require ('express')
 const path = require ('path')
 const fs = require ('fs')
@@ -5,10 +6,70 @@ const app = express ()
 
 var PORT = process.env.PORT || 3001;
 
+//setting middleware
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+  
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  
+  });
+  
+  app.post("/api/notes", function (req, res) {
+    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
+      if (error) {
+        return console.log(error)
+      }
+      notes = JSON.parse(notes)
+  
+      var id = notes[notes.length - 1].id + 1
+      var newNote = { title: req.body.title, text: req.body.text, id: id }
+      var activeNote = notes.concat(newNote)
+  
+      fs.writeFile(__dirname + "/db/db.json", JSON.stringify(activeNote), function (error, data) {
+        if (error) {
+          return error
+        }
+        res.json(activeNote);
+      })
+    })
+  })
+  
+  // Pull from db.json
+  app.get("/api/notes", function (req, res) {
+    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, data) {
+      if (error) {
+        return console.log(error)
+      }
+      console.log("This is Notes", data)
+      res.json(JSON.parse(data))
+    })
+  });
+  
+  app.put("/api/notes/:id", function(req, res) {
+    const noteId = JSON.parse(req.params.id)
+    fs.readFile(__dirname + "db/db.json", "utf8", function(error, notes) {
+      if (error ){
+        return console.log(error)
+      }
+      notes.JSONparse(notes)
+  
+      notes = notes.filter(val => val.id !== noteId)
+  
+      fs.writeFile(__dirname +"db/db.json", JSON.stringify(notes), function (error, data) {
+        if (error) {
+          return error
+        }
+        res.json(notes)
+      })
+    })
+  })
 
 
 
