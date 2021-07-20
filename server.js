88b418b84497af915ce606ba00d6ae6cc35a3b68
Jeff Grant <jgrant274@gmail.com>
2021-07-19 23:54:55 -0400

    code upload

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-//setting dependencies
+
 const express = require ('express')
 const path = require ('path')
 const fs = require ('fs')
-const app = express ()
 
+//setting up express app
+const app = express ()
 var PORT = process.env.PORT || 3001;
 
-//setting middleware
+// middleware
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-
+//html routes
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
   });
@@ -21,6 +22,7 @@ app.get("/", function (req, res) {
   
   });
   
+  //stores the note
   app.post("/api/notes", function (req, res) {
     fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
       if (error) {
@@ -29,50 +31,30 @@ app.get("/", function (req, res) {
       notes = JSON.parse(notes)
   
       var id = notes[notes.length - 1].id + 1
-      var newNote = { title: req.body.title, text: req.body.text, id: id }
-      var activeNote = notes.concat(newNote)
+      var nNote = { title: req.body.title, text: req.body.text, id: id }
+      var aNote = notes.concat(nNote)
   
-      fs.writeFile(__dirname + "/db/db.json", JSON.stringify(activeNote), function (error, data) {
+      fs.writeFile(__dirname + "/db/db.json", JSON.stringify(aNote), function (error, data) {
         if (error) {
           return error
         }
-        res.json(activeNote);
+        console.log(aNote)
+        res.json(aNote);
       })
     })
   })
   
-  // Pull from db.json
+ //retrieves the note
   app.get("/api/notes", function (req, res) {
     fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, data) {
       if (error) {
         return console.log(error)
       }
-      console.log("This is Notes", data)
+      console.log("This is a note", data)
       res.json(JSON.parse(data))
     })
   });
-  
-  app.put("/api/notes/:id", function(req, res) {
-    const noteId = JSON.parse(req.params.id)
-    fs.readFile(__dirname + "db/db.json", "utf8", function(error, notes) {
-      if (error ){
-        return console.log(error)
-      }
-      notes.JSONparse(notes)
-  
-      notes = notes.filter(val => val.id !== noteId)
-  
-      fs.writeFile(__dirname +"db/db.json", JSON.stringify(notes), function (error, data) {
-        if (error) {
-          return error
-        }
-        res.json(notes)
-      })
-    })
-  })
-
-
 
 app.listen(PORT, function () {
-    console.log("App listening on PORT" + PORT)
+    console.log("The app listening on PORT, " + PORT)
 })
